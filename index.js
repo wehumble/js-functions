afcf158484b9199b08c0bf5dcfb0c58f7cf92732
wehumble <otienostewart62@gmail.com>
2025-12-24 10:06:32 -0800

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+// Define the createLoginTracker function
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+  const correctPassword = userInfo.password;
+  
+  // Return the inner arrow function
+  return (passwordAttempt) => {
+    attemptCount++;
+    
+    if (attemptCount > 3) {
+      return 'Account locked due to too many failed login attempts';
+    } else if (passwordAttempt === correctPassword) {
+      return 'Login successful';
+    } else {
+      return `Attempt ${attemptCount}: Login failed`;
+    }
+  };
+}
 
-
-
-module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
-};
+// Export the function - USE THIS SIMPLE EXPORT
+module.exports = { createLoginTracker };
